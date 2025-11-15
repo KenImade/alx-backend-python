a6867aec17e7d8bc7795a842fdb62bf86e68be1a
Kenneth Imade <kenneth.imade@yahoo.com>
2025-11-15 11:47:12 +0000

    setup test for memoize function

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -6,7 +6,7 @@ import unittest
 from unittest.mock import patch, Mock
 from parameterized import parameterized
 from typing import Mapping, Sequence, Any
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -49,6 +49,32 @@ class TestGetJson(unittest.TestCase):
 
             mock_get.assert_called_once_with(test_url)
             self.assertEqual(result, test_payload)
+
+
+class TestMemoize(unittest.TestCase):
+    """Tests for memoizing decorator"""
+
+    def test_memoize(self) -> None:
+        """Test that memoize caches the result"""
+
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass, "a_method", return_value=42) as mock_method:
+            obj = TestClass()
+
+            # First call should call a_method
+            self.assertEqual(obj.a_property(), 42)
+            # Second call should use cached value
+            self.assertEqual(obj.a_property(), 42)
+
+            mock_method.assert_called_once()
+
 
 if __name__ == "__main__":
     unittest.main()
