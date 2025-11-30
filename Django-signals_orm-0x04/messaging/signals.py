9dca6574281e16e719fd076dafa45e7c373f3b60
Kenneth Imade <kenneth.imade@yahoo.com>
2025-11-30 20:14:35 +0000

    added signal function

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -31,4 +31,47 @@ def create_notification_on_new_message(sender, instance, created, **kwargs):
         print(f"--- Notification created for {receiver_user.username} ---")
 
 
-# Note: The signals must be connected when the app is ready, which is done in apps.py
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+from django.core.exceptions import ObjectDoesNotExist
+from .models import Message, MessageHistory
+
+
+@receiver(pre_save, sender=Message)
+def log_message_history_on_edit(sender, instance, **kwargs):
+    """
+    Listens for a Message instance about to be saved (updated) and logs
+    its previous content into the MessageHistory model.
+    """
+    # Check if this instance already exists in the database (i.e., it's an update, not a creation)
+    if instance.pk:
+        try:
+            # Get the current version of the object from the database
+            old_message = Message.objects.get(pk=instance.pk)
+
+            # Check if the content has actually changed
+            if old_message.content != instance.content:
+
+                # 1. Log the previous content
+                MessageHistory.objects.create(
+                    message=instance,
+                    old_content=old_message.content,
+                    # We assume the editor is the original sender for simplicity
+                    # in this model-only example. In a real view, you'd pass
+                    # the request.user to save() or the signal.
+                    editor=instance.sender,
+                )
+
+                # 2. Update the 'edited' field on the Message instance before it's saved
+                instance.edited = True
+
+                print(
+                    f"--- PRE_SAVE: Content change detected for Message {instance.pk}. Old content logged. ---"
+                )
+
+        except ObjectDoesNotExist:
+            # This should generally not happen if pk exists, but good practice to catch
+            print(f"--- Warning: Object {instance.pk} not found during pre_save. ---")
+    else:
+        # This is a new message creation, do nothing.
+        print(f"--- PRE_SAVE: New Message creation. Skipping history log. ---")
